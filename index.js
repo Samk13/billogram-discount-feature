4c7e5e07e57a0b1851a9351f5d4a24e4df91430b
Samk13 <samarbid13@gmail.com>
2021-06-24 13:50:17 +0200

    add swagger

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
-"use strict"
-const express = require("express")
-const app = express()
-const { router } = require("./src/router/router")
-const port = process.env.PORT || 3000
+'use strict';
+const express = require('express');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
+const { router } = require('./src/router/router');
+
+const swaggerOptions = {
+  apis: ['./src/router/*.js'],
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Billogram discount code API',
+      contact: {
+          email: "samarbid13@gmail.com",
+      },
+      version: '1.0.0',
+      description: 'A simple Express API that generate disount codes',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+};
+const specs = swaggerJsDoc(swaggerOptions)
+
+const port = process.env.PORT || 3000;
+const app = express();
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
 
 // to access url encoded request to read the arguments easily
-app.use(express.urlencoded({
-    extended: true
-})).listen(port)
+app
+  .use(
+    express.urlencoded({
+      extended: true,
+    })
+  )
+  .listen(port);
 
-app.use("/api", router)
+app.use('/api', router);
 
 // get requests
 app.get('/', (req, res) => {
-    res.sendFile('./src/pages/home.html',{root: __dirname})
-})
-
-
+  res.sendFile('./src/pages/home.html', { root: __dirname });
+});
 
 // redirect
 
 app.use((req, res) => {
-    res.redirect('/')
-})
+  res.redirect('/');
+});
--- src/router/router.js
+++ src/router/router.js
@@ -2,15 +2,53 @@ const express = require('express');
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *      schemas:
+*           code:
+*               type: object
+*               required:
+*                   - id
+*                   - code
+*               properties:
+*                   id:
+*                   type: string
+*                   description: the auto-generated id of the discount code
+                example:
+                    id: jh2342jklh3
+                    code: brandjh2342jklh3
+ *
+ */
+
+/**
+ * @swagger
+ * tags:
+ *      name: Brand
+ *      description: show discount code generated
+ */
+
+/**
+ *
+ *@swagger
+ * /api:
+ *   get:
+ *      summery: Returns the list of generated codes
+ *      tags: [Brand]
+ *      response:
+ *      200:
+ *          description: should response with json data with all codes available, we will not do pagination for now
+ *      content:
+ *      application/json:
+ *      schema:
+ *          type: array
+ *          items:
+ *              @ref: "#/component/schema/codes"
+ */
 router.get('/', (req, res) => {
     res.json({data: "hello! from the api"})
     // res.sendFile('../pages/home.html',{root: __dirname})
 })
-
-router.post('/getData', (req, res) => {
-  const { page_number } = req.query;
-  res.json(page_number);
-});
 
 router.get('/client', (req, res) => {
     res.json({
