3a92009fe2d496e157d64907a38bcfe3d29e46c8
Sam Arbid <sam.arbid@devcode.se>
2021-06-26 01:03:31 +0200

    testing with services

--- src/api/controllers/brand-controller.js
+++ src/api/controllers/brand-controller.js
@@ -1,8 +1,19 @@
+const {generateRandomNum, discountCodeGen, validateAmount, generateCode} = require("../../services/code-generator")
 exports.brand_greeting = (req, res) => {
+
   res.json({
       data: {
-         "greeting": "hello! from the api",
-         "time": new Date(Date.now())
+         greeting: "hello! from the api",
+         time: new Date(Date.now()),
+         codeAmount: discountCodeGen(5 , generateCode({
+            randFunc: generateRandomNum().toString(),
+            brandName:"Sam",
+            discount: "50%",
+            validDate: new Date(Date.now()),
+            user: "UserSam"})
+            ),
+         validate : validateAmount(123),
+         discountCode: generateCode({brandName:"Sam", discount: "50%",validDate: new Date(Date.now()), user: "UserSam"})
       }
    })
 }
