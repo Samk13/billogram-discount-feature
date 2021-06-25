11e4f902a178c9fdbcc24527db0e35442423aa68
Sam Arbid <sam.arbid@devcode.se>
2021-06-26 00:39:21 +0200

    add dotenv

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
+require('dotenv').config()
 const express = require('express');
 const { router } = require('./src/routes/router');
-
 
 const { swaggerOptions } = require("./src/config/swagger-config");
 const swaggerUI = require('swagger-ui-express');
@@ -11,7 +11,7 @@ const specs = swaggerJsDoc(swaggerOptions)
 const app = express();
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 // to access url encoded request to read the arguments easily
 app.use(
     express.urlencoded({
